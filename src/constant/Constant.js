2b07f2a137170726f2eb643723fdd5ce35be8759
vinjhun <jhunseng21@gmail.com>
2022-07-14 21:11:31 +0800

    add sign in on firebase

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -7,7 +7,7 @@ const Constant = {
   MAIN_ROUTES: [
     { path: 'story', name: 'Story', Component: Story },
     { path: 'item', name: 'Item', Component: Item },
-    { path: 'sign-in', name: 'Sign In', Component: SignIn },
+    { path: 'sign-in', name: 'Profile', Component: SignIn },
   ],
 };
 
--- src/pages/signin/signin.jsx
+++ src/pages/signin/signin.jsx
@@ -1,10 +1,22 @@
+import { signInGoogleWithPopUp } from '../../util/firebase/firebase-auth';
 
 const SignIn = () => {
-    return (
-        <div>
-            <h1>I am SignIn Page</h1>
-        </div>
-    )
+  const logIn = async () => {
+    console.log('test');
+    const response = await signInGoogleWithPopUp();
+    console.log(response);
+  };
+
+  const test = async () => {
+    console.log("click");
+  };
+
+  return (
+    <div>
+      <h1>I am SignIn Page</h1>
+      <button onClick={logIn} {...{className:'btn'}}>Sign In With Google Pop Up</button>
+    </div>
+  );
 };
 
 export default SignIn;
